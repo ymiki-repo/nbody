d1281c9fc0b7e89b154ffdb6c3e525ae070fab74
Yohei MIKI <ymiki@cc.u-tokyo.ac.jp>
2023-05-15 11:40:53 +0900

    bug fix

--- cpp/pragma/pragma_gpu_acc.hpp
+++ cpp/pragma/pragma_gpu_acc.hpp
@@ -10,42 +10,46 @@
 #ifndef PRAGMA_GPU_ACC_HPP
 #define PRAGMA_GPU_ACC_HPP
 
+#ifndef PRAGMA
+///
+/// @brief input arguments into _Pragma
+///
+#define PRAGMA(...) _Pragma(#__VA_ARGS__)
+#endif  // PRAGMA
+
+///
+/// @brief add arguments into _Pragma("acc")
+///
+#define PRAGMA_ACC(...) PRAGMA(acc __VA_ARGS__)
+
 ///
 /// @brief offload the specified loop as thread-blocks with n threads
 ///
-#define PRAGMA_ACC_OFFLOAD_THREAD(n)            \
-  {                                             \
-    _Pragma(acc kernels)                        \
-        _Pragma(acc loop independent vector(n)) \
-  }
+#define PRAGMA_ACC_OFFLOAD_THREAD(n) PRAGMA_ACC(kernels) PRAGMA_ACC(loop independent vector(n))
 
 ///
-/// @brief offload the specified loop as thread-blocks
+/// fff@brief offload the specified loop as thread-blocks
 ///
-#define PRAGMA_ACC_OFFLOAD            \
-  {                                   \
-    _Pragma(acc kernels)              \
-        _Pragma(acc loop independent) \
-  }
+#define PRAGMA_ACC_OFFLOAD PRAGMA_ACC(kernels) PRAGMA_ACC(loop independent)
 
 ///
 /// @brief allocate device memory
 ///
-#define PRAGMA_ACC_MALLOC(...) _Pragma(acc enter data create(__VA_ARGS__))
+#define PRAGMA_ACC_MALLOC(...) PRAGMA_ACC(enter data create(__VA_ARGS__))
 
 ///
 /// @brief release device memory
 ///
-#define PRAGMA_ACC_FREE(...) _Pragma(acc exit data delete (__VA_ARGS__))
+#define PRAGMA_ACC_FREE(...) PRAGMA_ACC(exit data delete(__VA_ARGS__))
 
 ///
 /// @brief memcpy from device to host
 ///
-#define PRAGMA_ACC_MEMCPY_D2H(...) _Pragma(acc update host(__VA_ARGS__))
+#define PRAGMA_ACC_MEMCPY_D2H(...) PRAGMA_ACC(update host(__VA_ARGS__))
 
 ///
 /// @brief memcpy from host to device
 ///
-#define PRAGMA_ACC_MEMCPY_H2D(...) _Pragma(acc update device(__VA_ARGS__))
+#define PRAGMA_ACC_MEMCPY_H2D(...) PRAGMA_ACC(update device(__VA_ARGS__))
 
 #endif  // PRAGMA_GPU_ACC_HPP
--- cpp/pragma/pragma_gpu_omp.hpp
+++ cpp/pragma/pragma_gpu_omp.hpp
@@ -10,7 +10,7 @@
 #ifndef PRAGMA_GPU_OMP_HPP
 #define PRAGMA_GPU_OMP_HPP
 
-// nvc++ (_OpenMP 202011) has #pragma omp target loop
+// nvc++ (_OPENMP 202011) has #pragma omp target loop
 // amdclang++ (_OPENMP 201811) does not have #pragma omp target loop
 #if _OPENMP >= 202011
 #define OMP_TARGET_HAS_LOOP_DIRECTIVE true
@@ -18,38 +18,55 @@
 #define OMP_TARGET_HAS_LOOP_DIRECTIVE false
 #endif
 
+#ifndef PRAGMA
+///
+/// @brief input arguments into _Pragma
+///
+#define PRAGMA(...) _Pragma(#__VA_ARGS__)
+#endif  // PRAGMA
+
+///
+/// @brief add arguments into _Pragma("omp")
+///
+#define PRAGMA_OMP(...) PRAGMA(omp __VA_ARGS__)
+
+///
+/// @brief add arguments into _Pragma("omp target")
+///
+#define PRAGMA_OMP_TARGET(...) PRAGMA_OMP(target __VA_ARGS__)
+
 ///
 /// @brief offload the specified loop as thread-blocks with n threads
 ///
-#define PRAGMA_OMP_TARGET_OFFLOAD_THREAD(n) _Pragma(omp target teams distribute parallel for simd thread_limit(n))
+#define PRAGMA_OMP_TARGET_OFFLOAD_THREAD(n) PRAGMA_OMP_TARGET(teams distribute parallel for simd thread_limit(n))
 
 ///
 /// @brief offload the specified loop as thread-blocks
 ///
 #if OMP_TARGET_HAS_LOOP_DIRECTIVE
-#define PRAGMA_OMP_TARGET_OFFLOAD _Pragma(omp target teams loop)
+#define PRAGMA_OMP_TARGET_OFFLOAD PRAGMA_OMP_TARGET(teams loop)
 #else  // OMP_TARGET_HAS_LOOP_DIRECTIVE
-#define PRAGMA_OMP_TARGET_OFFLOAD _Pragma(omp target teams distribute parallel for simd)
+#define PRAGMA_OMP_TARGET_OFFLOAD PRAGMA_OMP_TARGET(teams distribute parallel for simd)
 #endif  // OMP_TARGET_HAS_LOOP_DIRECTIVE
 
 ///
 /// @brief allocate device memory
 ///
-#define PRAGMA_OMP_TARGET_MALLOC(...) _Pragma(omp enter data map(alloc \
-                                                                 : __VA_ARGS__))
+#define PRAGMA_OMP_TARGET_MALLOC(...) PRAGMA_OMP_TARGET(enter data map(alloc \
+                                                                       : __VA_ARGS__))
 ///
 /// @brief release device memory
 ///
-#define PRAGMA_OMP_TARGET_FREE(...) _Pragma(omp exit data map(delete \
-                                                              : __VA_ARGS__))
+#define PRAGMA_OMP_TARGET_FREE(...) PRAGMA_OMP_TARGET(exit data map(delete \
+                                                                    : __VA_ARGS__))
 ///
 /// @brief memcpy from device to host
 ///
-#define PRAGMA_OMP_TARGET_MEMCPY_D2H(...) _Pragma(omp target update from(__VA_ARGS__))
+#define PRAGMA_OMP_TARGET_MEMCPY_D2H(...) PRAGMA_OMP_TARGET(update from(__VA_ARGS__))
 
 ///
 /// @brief memcpy from host to device
 ///
-#define PRAGMA_OMP_TARGET_MEMCPY_H2D(...) _Pragma(omp target update to(__VA_ARGS__))
+#define PRAGMA_OMP_TARGET_MEMCPY_H2D(...) PRAGMA_OMP_TARGET(update to(__VA_ARGS__))
 
 #endif  // PRAGMA_GPU_OMP_HPP
